faf2ed740083d754b94c7b37cbd6ae24e2a6c3ef
Andrew Hudgins <andrewhudgins15@gmail.com>
2022-08-07 15:36:55 -0500

    Can post data to file and populates notes from file from a get command

--- routes/apiRoutes/noteRoutes.js
+++ routes/apiRoutes/noteRoutes.js
@@ -1,7 +1,20 @@
 const router = require("express").Router();
 const db = require('../../db/db.json');
+const { createNewNote } = require("../../lib/notes");
 
+router.get('/notes', (req, res) => {
+    // Log our request to the terminal
+    console.info(`${req.method} request received to get reviews`);
 
+    // Send a message to the client
+    res.json(db);
+});
+
+router.post('/notes', (req, res) => {
+    // Log that a POST request was received
+    console.info(`${req.method} request received to add a review`);
+    createNewNote(req.body)
+});
 
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const htmlRoutes = require('./routes/htmlRoutes');
 const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
 
 const PORT = process.env.PORT || 3001;
 
@@ -9,8 +9,8 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
-app.use('/', htmlRoutes);
 app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}.`);
